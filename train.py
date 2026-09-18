e9511e73556cfb3c06a6c2eb7e3af3fb70b5d95b
爱学习的溜溜 <lty09637@gmail.com>
2026-09-19 01:33:44 +0800

    Update train.py

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     device = os.getenv('HCD_DEVICE', '0' if torch.cuda.is_available() else 'cpu')
 
     model = RTDETR(str(MODEL_CONFIG))
-    # model.load('/data1/liutianyi/RTDETR-main-2/runs/train/exp2/weights/best.pt') # loading pretrain weights
+
     model.train(data=data,
                 cache=False,
                 imgsz=int(os.getenv('HCD_IMGSZ', '640')),
@@ -26,5 +26,5 @@ if __name__ == '__main__':
                 device=device,
                 # resume='true', # last.pt path
                 project=os.getenv('HCD_PROJECT', 'runs/lambda'),
-                name=os.getenv('HCD_NAME', '去掉cfo-visdrone2019'),
+                name=os.getenv('HCD_NAME', 'hcd'),
                 )
